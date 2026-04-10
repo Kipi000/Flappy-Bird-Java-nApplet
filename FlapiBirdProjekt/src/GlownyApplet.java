68144c3c9dbc4d83fc38212eabe78dea0a713ec6
Kipi000 <105994038+Kipi000@users.noreply.github.com>
2026-04-10 23:49:56 +0200

    opadanie i rysowanie
    spoiler: uklad wspolrzednych Y w javie jest zjebany i dziala na odwort (im nizej w dol tym wartosc bardziej dodatnia)

--- FlapiBirdProjekt/src/GlownyApplet.java
+++ FlapiBirdProjekt/src/GlownyApplet.java
@@ -1,16 +1,49 @@
 import javax.swing.JApplet;
+import javax.swing.Timer;
+import java.awt.*;
+import java.awt.event.*;
 
-public class GlownyApplet extends JApplet {
+public class GlownyApplet extends JApplet implements ActionListener{
+	int ruraWys = 150;
+	int ruraX = 400;
+	int przerwa = 150;
+	
+	int ptakY = 200;
+	int ptakV = 0;
+	
+	Timer timer;
+	Image img;
 	@Override
 	public void init() {
-	    // Tu wpisujesz to, co ma się stać przy starcie apletu
-	    System.out.println("Aplet zainicjowany!");
+		setSize(500, 500);
+		img = getImage(getDocumentBase(), "ptak.png");
+		timer = new Timer(20, this);
+        timer.start();
 	}
 
 	@Override
 	public void paint(java.awt.Graphics g) {
-	    // Tu rysujesz grafikę
-	    g.drawRect(50, 50, 100, 100); // Rysuje kwadrat
-	    g.drawString("Mój pierwszy aplet", 60, 80);
+		g.setColor(new Color(77, 129, 249));
+		g.fillRect(0, 0, getWidth(), getHeight());
+		g.drawImage(img, 30, ptakY, 45, 35, this);
+		g.setColor(Color.green);
+		g.fillRect(ruraX, 0, 50, ruraWys);
+		g.fillRect(ruraX, ruraWys+przerwa, 50, getHeight());
+	}
+	public void actionPerformed(ActionEvent e) {
+		ptakV += 1;
+		ptakY += ptakV;
+		
+		ruraX -= 5;
+		
+		if(ruraX + 50 < 0) {
+			ruraX = getWidth();
+		}
+		
+		if (ptakY > getHeight()-20) {
+			ptakY = getHeight()-20;
+			ptakV = 0;
+		}
+		repaint();
 	}
 }
